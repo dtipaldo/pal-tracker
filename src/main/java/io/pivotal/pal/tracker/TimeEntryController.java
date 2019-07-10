e4f1726aaf14678e3d065662280d89baff255e7f
Dan Tipaldo <dtipaldo@gmail.com>
2019-07-10 09:51:04 -0400

    Implement CRUD API with inMemory

---
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -0,0 +1,50 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.sql.Time;
+import java.util.List;
+
+@RestController
+@RequestMapping("/time-entries")
+public class TimeEntryController {
+    private TimeEntryRepository timeEntryRepository;
+
+    public TimeEntryController(TimeEntryRepository timeEntryRepository) {
+        this.timeEntryRepository = timeEntryRepository;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<TimeEntry> create(@RequestBody TimeEntry timeEntryToCreate) {
+        TimeEntry body = this.timeEntryRepository.create(timeEntryToCreate);
+        return ResponseEntity.status(HttpStatus.CREATED).body(body);
+    }
+
+    @GetMapping("/{timeEntryId}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<TimeEntry> read(@PathVariable Long timeEntryId) {
+        TimeEntry timeEntry = this.timeEntryRepository.find(timeEntryId);
+        return (timeEntry == null) ? ResponseEntity.notFound().build() : ResponseEntity.ok().body(timeEntry);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TimeEntry>> list() {
+        return ResponseEntity.ok().body(this.timeEntryRepository.list());
+    }
+
+    @PutMapping("/{timeEntryId}")
+    public ResponseEntity update(@PathVariable Long timeEntryId, @RequestBody TimeEntry timeEntry) {
+        TimeEntry timeEntryResponse = this.timeEntryRepository.update(timeEntryId, timeEntry);
+        return (timeEntryResponse == null) ? ResponseEntity.notFound().build() : ResponseEntity.ok().body(timeEntryResponse);
+    }
+
+    @DeleteMapping("/{timeEntryId}")
+    public ResponseEntity delete(@PathVariable long timeEntryId) {
+        this.timeEntryRepository.delete(timeEntryId);
+        return ResponseEntity.noContent().build();
+    }
+}
